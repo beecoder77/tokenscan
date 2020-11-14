913e8a7fbd4e5f226580983e752e39d86d565997
muhsalmanabid <muhsalmanabid@gmail.com>
2020-11-14 15:50:24 +0700

    add bscscan

--- check.php
+++ check.php
@@ -4,25 +4,48 @@ require 'art.php';
 
 awal:
 echo $art;
-echo $menu;
+echo $mainMenu;
 echo "Choose Menu = ";
 $pilihan = trim(fgets(STDIN));
 switch($pilihan){
     case "1":
         cls();
         echo $art;
-        checkAddress();
-        echo $ulang;
-        $answer = trim(fgets(STDIN));
-        if(strtolower($answer) === 'y'){
-            cls();
-            goto awal;
+        echo $menuEtherScan;
+        echo "Choose Menu = ";
+        $pilihanEther = trim(fgets(STDIN));
+        switch($pilihanEther){
+            case "1":
+                cls();
+                echo $art;
+                checkAddressETH();
+                echo $ulang;
+                $answer = trim(fgets(STDIN));
+                if(strtolower($answer) === 'y'){
+                    cls();
+                    goto awal;
+                }
+            break;
+            case "2": 
+                cls();
+                echo $art;
+                checkBalanceETH();
+                echo $ulang;
+                $answer = trim(fgets(STDIN));
+                if(strtolower($answer) === 'y'){
+                    cls();
+                    goto awal;
+                }
+            break;
+            default:
+                cls();
+                echo "Menu tidak tersedia!";
         }
     break;
     case "2": 
         cls();
         echo $art;
-        checkBalance();
+        checkBalanceBSC();
         echo $ulang;
         $answer = trim(fgets(STDIN));
         if(strtolower($answer) === 'y'){
--- function.php
+++ function.php
@@ -1,16 +1,16 @@
 <?php
-$API_KEY_TOKEN = 'IBQTT7811MYYGPYU4NQ2RK7U6DQACAAJBG';
+$API_KEY_TOKEN = 'TOKENMU';
 
 function cls()                                                                                                             
 {
     print("\033[2J\033[;H");
 }
 
-function checkAddress(){
-    $address = file('address.txt');
-    print PHP_EOL.count(explode("\n",str_replace("\r", "", file_get_contents('address.txt'))))." address\n______________________________\n";
+function checkAddressETH(){
+    $address = file_get_contents('address.txt');
+    print PHP_EOL.count(explode("\n",str_replace("\r", "", $address)))." address\n______________________________\n";
     foreach(explode(PHP_EOL, file_get_contents('address.txt')) as $value){
-        $check = getAddress($value);
+        $check = getAddressETH($value);
         if($check === 'OK'){
             echo 'LIVE|'.$value."\n";
         } elseif ($check === 'No transactions found'){
@@ -19,21 +19,21 @@ function checkAddress(){
     }
 }
 
-function checkBalance(){
+function checkBalanceETH(){
     $address = file_get_contents('address.txt');
-    print PHP_EOL.count(explode("\n",str_replace("\r", "", file_get_contents('address.txt'))))." address\n______________________________\n";
+    print PHP_EOL.count(explode("\n",str_replace("\r", "", $address)))." address\n______________________________\n";
     $address = preg_replace('/\s+/',',',str_replace(array("\r\n","\r","\n"),' ',trim($address)));
-    $check = getBalance($address);
+    $check = getBalanceETH($address);
     foreach($check as $result){
         if($result['balance'] === '0'){
             echo 'DIE|'.$result['account']."\n";
         } else {
-            echo 'LIVE|'.$result['account']."  => balance=".$result['balance']."\n";
+            echo 'LIVE|'.$result['account']."  => BALANCE =".$result['balance']."\n";
         }
     };
 }
 
-function getAddress($address) {
+function getAddressETH($address) {
     global $API_KEY_TOKEN;
     $curl = curl_init();
 
@@ -55,12 +55,50 @@ function getAddress($address) {
     return $response['message'];
 }
 
-function getBalance($address){
+function getBalanceETH($address){
     global $API_KEY_TOKEN;
     $curl = curl_init();
 
     curl_setopt_array($curl, array(
     CURLOPT_URL => "https://api.etherscan.io/api?module=account&action=balancemulti&address=".$address."&tag=latest&apikey=".$API_KEY_TOKEN,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+
+    $response = json_decode($response, true);
+    return $response['result'];
+}
+
+function checkBalanceBSC(){
+    $address = file_get_contents('address.txt');
+    print PHP_EOL.count(explode("\n",str_replace("\r", "", $address)))." address\n______________________________\n";
+    $address = explode("\n",$address);
+    foreach($address as $address){
+        $check = getBalanceBSC($address);
+        if($check === 'Invalid address format'){
+            echo 'DIE|ADDRESS NOT VALID = '.$address."\n";
+        } elseif($check === '0'){
+            echo 'DIE|'.$address.'|BALANCE = '.$check."\n";
+        } else {
+            echo 'LIVE|'.$address.'|BALANCE = '.$check."\n";
+        }
+    }
+}
+
+function getBalanceBSC($address){
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x4247aeb8e759e575fe350921cd174c48df304f2a&address=".$address."&tag=latest",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
